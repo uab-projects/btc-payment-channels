0f6bb2f591d794d8f44708ed939afa5680a7cc4b
Carlos González Cebrecos <c.cebrecos@hotmail.com>
2017-02-10 11:30:42 +0100

    Template files added
    Initial skeleton

---
+++ src/cli/arguments/constants.py
@@ -0,0 +1,8 @@
+
+#Log level
+"""
+Default log level
+"""
+LOGS = ["debug","info","warning","error","critical"]
+LOGS_LEVELS = [10,20,30,40,50]
+LOG_DEFAULT = LOGS[1]
---
+++ src/main.py
@@ -0,0 +1,47 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+#Libraries
+import os
+import sys
+import platform
+import logging
+import core.log
+
+from cli.arguments.parser import DEFAULT_PARSER
+from cli.arguments.constants import *
+from core.data.parser import *
+
+
+#Constants
+LOGGER = logging.getLogger(__name__)
+
+# variables
+"""
+Arguments namespace
+"""
+args = None
+
+#Functions
+"""
+Takes the system arguments vector and tries to parse the arguments in it given
+the argument parser specified and returns the namespace generated
+
+@param 	parser 	the ArgumentParser objects to use to parse the arguments
+@param 	args 	arguments to parse (default is sys.argv)
+@return namespace of parsed arguments
+"""
+def parseArguments(parser, args=None):
+	return parser.parse_args(args)
+
+if __name__ == "__main__":
+	# Prepare coding
+	if platform.system() == "Windows":
+		os.system("chcp 65001")
+	args = parseArguments(DEFAULT_PARSER)
+	# Switching log level
+	root_logger = logging.getLogger()
+	root_logger.setLevel(LOGS_LEVELS[LOGS.index(args.log_level)])
+	#Welcome
+	LOGGER.info("Welcome !")
+	LOGGER.info("Bye !")
