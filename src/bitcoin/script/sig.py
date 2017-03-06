c241e70b9b0340c620ba2a2a27686c3fefaa14aa
ccebrecos <c.cebrecos@hotmail.com>
2017-03-06 14:21:28 +0100

    Skeleton to scriptsig

--- src/bitcoin/script/sig.py
+++ src/bitcoin/script/sig.py
@@ -1,7 +1,24 @@
 """
 """
 from .general import Script
+from ..io.input import TxInput
 
 
 class ScriptSig(Script):
-    pass
+    """
+    """
+    __slots__ = ["_input"]
+
+    def __init__(self):
+        super().__init__()
+
+    def sign(self, privKey):
+        """
+        Description
+
+        Args:
+            privKey(Address): private key that will be used to sign the current
+            transaction, that signature will be part of the transaction and the
+            script.
+        """
+        pass
