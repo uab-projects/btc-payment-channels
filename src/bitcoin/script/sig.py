e26d303fcefedb478d467221c176ad8543d0da16
Carlos GC <c.cebrecos@hotmail.com>
2017-03-06 22:59:02 +0100

    Some shit done, raw idea

--- src/bitcoin/script/sig.py
+++ src/bitcoin/script/sig.py
@@ -1,24 +1,43 @@
 """
+More information could be found in:
+https://bitcoin.org/en/developer-guide#standard-transactions
 """
 from .general import Script
-from ..io.input import TxInput
+from ..field.script import StackDataField
 
 
 class ScriptSig(Script):
     """
     """
-    __slots__ = ["_input"]
-
     def __init__(self):
         super().__init__()
 
-    def sign(self, privKey):
-        """
-        Description
 
-        Args:
-            privKey(Address): private key that will be used to sign the current
-            transaction, that signature will be part of the transaction and the
-            script.
-        """
-        pass
+class P2PKH(ScriptSig):
+    __slots__ = ["_address", "_input"]
+
+    def __init__(self, address):
+        self._address = address
+        self._build()
+
+    def _build(self):
+        # Signature
+        self._data.append(StackDataField())
+        # PubKey
+        self._data.append(StackDataField())
+
+
+class P2SH(ScriptSig):
+    """
+    """
+    __slots__ = ["_reedemScript"]
+
+    def __init__(self, reedemScript):
+        self._reedemScript = reedemScript
+        self._build()
+
+    def _build(self):
+        # Signature(s)
+        while 1:
+            self._data.append(StackDataField())
+        self._data.append(StackDataField(self._reedemScript))
