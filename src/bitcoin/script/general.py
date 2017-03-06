ef477e5705f3f0d5c8b400806be3e392ef004197
ccebrecos <c.cebrecos@hotmail.com>
2017-03-06 12:45:13 +0100

    Implemented general script

--- src/bitcoin/script/general.py
+++ src/bitcoin/script/general.py
@@ -10,4 +10,44 @@ from ..interfaces import Serializable
 
 
 class Script(Serializable):
-    pass
+    """
+    Main scripting class of the application
+    """
+    __slots__ = ["_data"]
+
+    def __init__(self):
+        self._data = []
+
+    def serialize(self):
+        """
+        Serializes the contents of the current class into an array of bytes so
+        the class can be represented as an array of bytes compatible with what
+        the Bitcoin protocol specifies
+
+        Returns:
+            bytes: data of the class serialized in a bytes object
+        """
+        serialized = []
+        serialized = [x.serialize() for x in self._data]
+        return b''.join(serialized)
+
+    def deserialize(self, data):
+        """
+        Deserializes the contents of the data passed to try make them fit into
+        the class model. If the data has invalid length or invalid data,
+        appropiate exceptions will be raised.
+
+        Please implement this method in a way that can receive more data than
+        the strictly required in variable sized fields, so it will help caller
+        methods to detect size after calling deserialization
+
+        Args:
+            data (bytes): a bytes object containing data to de-serialize
+
+        Returns:
+            the instance of the class filled with the data if succeeded
+
+        Raises:
+            ValueError: if data can't be fit into the model
+        """
+        pass
