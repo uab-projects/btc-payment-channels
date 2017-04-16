5a5078c52f174e0490e430aeee26116616a44177
Carlos GC <c.cebrecos@hotmail.com>
2017-04-16 20:34:21 +0200

    Created multisig test

---
+++ src/tests/multisig.py
@@ -0,0 +1,87 @@
+import sys
+from bitcoin import main as vbuter
+from ..bitcoin.tx import Tx
+from ..bitcoin.io.input import TxInput
+from ..bitcoin.io.output import TxOutput
+from ..bitcoin import script
+from ..bitcoin import address
+from ..bitcoin.nets import Network
+from ..bitcoin.script import redeem
+from ..bitcoin.script.helper import prepare_tx_to_sign_multisig, sign_tx
+
+
+if __name__ == "__main__":
+
+    # Transaction fields
+    prev_tx = bytes().fromhex("""40d271f31d4249e4c58a952348dff8f211b71edbf646086
+                              b1faf17561f0696f9""")
+    prev_tx_out = 0
+    utxo_value = 0.11000000
+    fees = 0.00025
+    to_pay = utxo_value - fees
+    p2pkh_key, priv_key_0, priv_key_1 = sys.argv[1:4]
+    pubkey_0 = vbuter.privkey_to_pubkey(priv_key_0)
+    pubkey_1 = vbuter.privkey_to_pubkey(priv_key_1)
+
+    # Create transaction
+    transaction = Tx()
+
+    rS = redeem.MultiSig(2, 2)
+    rS.add_PubKey(bytes().fromhex(pubkey_0))
+    rS.add_PubKey(bytes().fromhex(pubkey_1))
+
+    # Add outputs
+    out_0 = TxOutput()
+    out_0.script = script.pubkey.P2SH(address.P2SH(Network.testnet, rS))
+    out_0.btc = to_pay
+    transaction.add_output(out_0)
+
+    # Add input
+    in_0 = TxInput(prev_tx, prev_tx_out)
+    transaction.add_input(in_0)
+    scriptsig = script.sig.P2PKH(in_0)
+    scriptsig.sign(p2pkh_key)
+
+    # Get transaction
+    print(transaction)
+    print(transaction.serialize().hex())
+
+    # --------------------------------------------------------------------------
+    # redeem the previous transaction
+    # --------------------------------------------------------------------------
+
+    # Transaction fields
+    prev_tx = transaction.id
+    to_pay = 0
+    to_ret = 0
+    to_pay_addr = ""
+    to_ret_addr = ""
+    priv_key_0 = ""
+    priv_key_1 = ""
+    pay_script = rS.pay_script
+
+    # Create transaction
+    redeem_tx = Tx()
+
+    # Add outputs
+    out_1 = TxOutput()
+    out_1.script = script.pubkey.P2PKH(address.P2PKH.decode(to_pay_addr))
+    out_1.btc = to_pay
+    transaction.add_output(out_1)
+
+    # Add inputs
+    in_1 = TxInput(prev_tx, 0)
+    transaction.add_input(in_1)
+    in_1.script = script.sig.P2SH(rS, pay_script)
+
+    # Make signatures
+    s_tx = prepare_tx_to_sign_multisig(redeem_tx, 0, out_0.script)
+    sig_0 = sign_tx(s_tx, priv_key_0)
+    sig_1 = sign_tx(s_tx, priv_key_1)
+    # Add signatures
+    pay_script.add_signature(sig_0)
+    pay_script.add_signature(sig_1)
+
+    # Get transaction
+    print(redeem_tx)
+    print(redeem_tx.serialize().hex())
