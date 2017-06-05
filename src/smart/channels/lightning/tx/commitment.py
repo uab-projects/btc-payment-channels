1a73afae9e713014653a72b00880ef3a40546a7f
Carlos GC <c.cebrecos@hotmail.com>
2017-06-05 23:29:16 +0200

    Started something

---
+++ src/smart/channels/lightning/tx/commitment.py
@@ -0,0 +1,16 @@
+"""
+Defines the class and all the things needed to open a payment channel following
+the Lightning Network specification
+"""
+
+from ......bitcoin import SignableTx
+
+
+class Commitment(SignableTx):
+    """
+    """
+
+    __slots__ = []
+
+    def __init__():
+        pass
